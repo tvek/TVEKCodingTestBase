9a82431f3cc259d2c9c88c5e42cfbc000afb6129
Thomas Vimal Easo K <thomasvml@gmail.com>
2023-01-20 00:31:41 +0900

    Create 0001. Alternating Traffic Signal Generator.cpp
    Question

---
+++ 0001. Alternating Traffic Signal Generator.cpp
@@ -0,0 +1,6 @@
+/***********************************************************************************************************
+* Program to print the out of a traffic signal.
+* Currently there are two signals in the system which can generate output alternatively in every 5 seconds.
+* For example. when signal#1 is Red, signal#2 should be green and alternatively.
+* Program should execute indefinitely until there is forced stop.
+************************************************************************************************************/
